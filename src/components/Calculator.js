0430b6ef9e4275e53ec3b6d9239a4876643faa93
Mussie Kahsay <mussiekahasay12@gmail.com>
2023-04-26 13:58:51 +0300

    Modify Calculator.js

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,13 +1,27 @@
-import React from 'react';
-import './Calculator.css';
-import Buttons from './Buttons';
+import React, { useState } from 'react';
+
 import Display from './Display';
+import Buttons from './Buttons';
+
+import './Calculator.css';
+import calculate from '../logic/calculate';
 
 function Calculator() {
+  const [state, setState] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
+
+  const handleClick = (buttonName) => {
+    const newState = calculate(state, buttonName);
+    setState(newState);
+  };
+
   return (
     <div className="calculator">
-      <Display />
-      <Buttons />
+      <Display value={state.next || state.total || '0'} />
+      <Buttons onClick={handleClick} />
     </div>
   );
 }
